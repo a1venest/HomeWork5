19ba9aa17dda3b42983c462b78dccc53e53a9abf
Aivenest <aivenest@gmail.com>
2022-10-06 12:53:11 +0500

    ex02 copleted

---
+++ Exercise02/Program.cs
@@ -0,0 +1,33 @@
+﻿void FillArray(int[] arr, int min, int max)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int CountNumber(int[] arr)
+{
+    int sum = 0;
+    for(int i =0 ;i< arr.Length;i++)
+    {
+        if(arr[i]%2>0)
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+int[] array = new int[20];
+FillArray(array, 1,9);
+PrintArray(array);
+
+Console.WriteLine("Сумма нечетных чисел равна: " + CountNumber(array));
